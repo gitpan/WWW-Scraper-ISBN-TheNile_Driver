7f9565fdfe91deb0e23a68cd48c990ed7cc65239
Barbie <barbie@missbarbell.co.uk>
2010-07-30 13:47:40 +0000

    Import of BARBIE/WWW-Scraper-ISBN-TheNile_Driver-0.01 from CPAN.
    gitpan-cpan-distribution: WWW-Scraper-ISBN-TheNile_Driver
    gitpan-cpan-version:      0.01
    gitpan-cpan-path:         BARBIE/WWW-Scraper-ISBN-TheNile_Driver-0.01.tar.gz
    gitpan-cpan-author:       BARBIE
    gitpan-cpan-maturity:     released

---
+++ lib/WWW/Scraper/ISBN/TheNile_Driver.pm
@@ -0,0 +1,188 @@
+package WWW::Scraper::ISBN::TheNile_Driver;
+
+use strict;
+use warnings;
+
+use vars qw($VERSION @ISA);
+$VERSION = '0.01';
+
+#--------------------------------------------------------------------------
+
+=head1 NAME
+
+WWW::Scraper::ISBN::TheNile_Driver - Search driver for TheNile online book catalog.
+
+=head1 SYNOPSIS
+
+See parent class documentation (L<WWW::Scraper::ISBN::Driver>)
+
+=head1 DESCRIPTION
+
+Searches for book information from TheNile online book catalog
+
+=cut
+
+#--------------------------------------------------------------------------
+
+###########################################################################
+# Inheritence
+
+use base qw(WWW::Scraper::ISBN::Driver);
+
+###########################################################################
+# Modules
+
+use WWW::Mechanize;
+
+###########################################################################
+# Constants
+
+use constant	SEARCH	=> 'http://www.thenile.com.au/search.php?s=';
+
+#--------------------------------------------------------------------------
+
+###########################################################################
+# Public Interface
+
+=head1 METHODS
+
+=over 4
+
+=item C<search()>
+
+Creates a query string, then passes the appropriate form fields to the TheNile
+server.
+
+The returned page should be the correct catalog page for that ISBN. If not the
+function returns zero and allows the next driver in the chain to have a go. If
+a valid page is returned, the following fields are returned via the book hash:
+
+  isbn          (now returns isbn13)
+  isbn10        
+  isbn13
+  ean13         (industry name)
+  author
+  title
+  book_link
+  image_link
+  description
+  pubdate
+  publisher
+  binding       (if known)
+  pages         (if known)
+  weight        (if known) (in grammes)
+  width         (if known) (in millimetres)
+  height        (if known) (in millimetres)
+
+The book_link and image_link refer back to the TheNile website.
+
+=back
+
+=cut
+
+sub search {
+	my $self = shift;
+	my $isbn = shift;
+	$self->found(0);
+	$self->book(undef);
+
+	my $mechanize = WWW::Mechanize->new();
+    $mechanize->agent_alias( 'Linux Mozilla' );
+	$mechanize->get( SEARCH . $isbn );
+
+    return $self->handler("TheNile website appears to be unavailable.")
+	    unless($mechanize->success());
+
+	# The Book page
+    my $html = $mechanize->content();
+
+	return $self->handler("Failed to find that book on TheNile website.")
+		if($html =~ m!We're sorry, but your search returned no results.!si);
+    
+#print STDERR "\n# content1=[\n$html\n]\n";
+
+    my $data;
+    ($data->{image})                    = $html =~ m!(http://tncdn.net/\d+/\d+/\d+/\d+.jpg)!i;
+    ($data->{thumb})                    = $html =~ m!(http://tncdn.net/\d+/\d+/\d+/\d+.jpg)!i;
+    ($data->{isbn13})                   = $html =~ m!<th>ISBN-13:</th>\s*<td>(\d+)</td>!i;
+    ($data->{isbn10})                   = $html =~ m!<th>ISBN:</th>\s*<td>(\d+)</td>!i;
+    ($data->{author})                   = $html =~ m!tr><th>Authors:</th><td>((?:<a[^>]+>[^<]+</a>[,\s]*)+)</td></tr>!i;
+    ($data->{title})                    = $html =~ m!<tr><th>Title:</th><td>([^,<]+)</td></tr>!i;
+    ($data->{publisher})                = $html =~ m!tr><th>Publisher:</th><td>((?:<a[^>]+>[^<]+</a>[,\s]*)+)</td></tr>!i;
+    ($data->{pubdate})                  = $html =~ m!<tr><th>Year:</th><td>([^<]+)</td></tr>!i;
+    ($data->{binding})                  = $html =~ m!<th>Format:</th>\s*<td>([^<]+)</td>!s;
+    ($data->{pages})                    = $html =~ m!<tr><th>Pages:</th><td>([\d.]+)</td></tr>!s;
+    ($data->{weight})                   = $html =~ m!<tr><th>Weight:</th><td>(\d+)g</td></tr>!s;
+    ($data->{width},$data->{height})    = $html =~ m!<tr><th>Dimensions:</th><td>([\d.]+)mm x ([\d.]+)mm</td></tr>!s;
+    ($data->{description})              = $html =~ m!<strong>Annotation</strong><br>([^<]+)!;
+    ($data->{description})              = $html =~ m!<strong>Publisher Description</strong><br>([^<]+)!    unless($data->{description});
+
+    $data->{author} =~ s!<[^>]+>!!g     if($data->{author});
+    $data->{publisher} =~ s!<[^>]+>!!g  if($data->{publisher});
+
+#use Data::Dumper;
+#print STDERR "\n# " . Dumper($data);
+
+	return $self->handler("Could not extract data from TheNile result page.")
+		unless(defined $data);
+
+	# trim top and tail
+	foreach (keys %$data) { next unless(defined $data->{$_});$data->{$_} =~ s/^\s+//;$data->{$_} =~ s/\s+$//; }
+
+	my $bk = {
+		'ean13'		    => $data->{isbn13},
+		'isbn13'		=> $data->{isbn13},
+		'isbn10'		=> $data->{isbn10},
+		'isbn'			=> $data->{isbn13},
+		'author'		=> $data->{author},
+		'title'			=> $data->{title},
+		'book_link'		=> $mechanize->uri(),
+		'image_link'	=> $data->{image},
+		'thumb_link'	=> $data->{thumb},
+		'description'	=> $data->{description},
+		'pubdate'		=> $data->{pubdate},
+		'publisher'		=> $data->{publisher},
+		'binding'	    => $data->{binding},
+		'pages'		    => $data->{pages},
+		'weight'		=> $data->{weight},
+		'width'		    => $data->{width},
+		'height'		=> $data->{height}
+	};
+
+#use Data::Dumper;
+#print STDERR "\n# book=".Dumper($bk);
+
+    $self->book($bk);
+	$self->found(1);
+	return $self->book;
+}
+
+1;
+__END__
+
+=head1 REQUIRES
+
+Requires the following modules be installed:
+
+L<WWW::Scraper::ISBN::Driver>,
+L<WWW::Mechanize>
+
+=head1 SEE ALSO
+
+L<WWW::Scraper::ISBN>,
+L<WWW::Scraper::ISBN::Record>,
+L<WWW::Scraper::ISBN::Driver>
+
+=head1 AUTHOR
+
+  Barbie, <barbie@cpan.org>
+  Miss Barbell Productions, <http://www.missbarbell.co.uk/>
+
+=head1 COPYRIGHT & LICENSE
+
+  Copyright (C) 2004-2010 Barbie for Miss Barbell Productions
+
+  This module is free software; you can redistribute it and/or
+  modify it under the Artistic Licence v2.
+
+=cut
---
+++ t/10object.t
@@ -0,0 +1,132 @@
+#!/usr/bin/perl -w
+use strict;
+
+use lib './t';
+use Test::More tests => 57;
+
+###########################################################
+
+use WWW::Scraper::ISBN;
+my $scraper = WWW::Scraper::ISBN->new();
+isa_ok($scraper,'WWW::Scraper::ISBN');
+
+SKIP: {
+	skip "Can't see a network connection", 56   if(pingtest());
+
+	$scraper->drivers("TheNile");
+
+    # this ISBN doesn't exist
+	my $isbn = "1234567890";
+    my $record;
+    eval { $record = $scraper->search($isbn); };
+    if($@) {
+        like($@,qr/Invalid ISBN specified/);
+    }
+    elsif($record->found) {
+        ok(0,'Unexpectedly found a non-existent book');
+    } else {
+		like($record->error,qr/Failed to find that book on TheNile website/);
+    }
+
+	$isbn = "0099547937";
+	$record = $scraper->search($isbn);
+
+    unless($record->found) {
+		diag($record->error);
+    } else {
+		is($record->found,1);
+		is($record->found_in,'TheNile');
+
+		my $book = $record->book;
+		is($book->{'isbn'},         '9780099547938'         ,'.. isbn found');
+		is($book->{'isbn10'},       '0099547937'            ,'.. isbn10 found');
+		is($book->{'isbn13'},       '9780099547938'         ,'.. isbn13 found');
+		is($book->{'ean13'},        '9780099547938'         ,'.. ean13 found');
+		is($book->{'title'},        'Ford County'           ,'.. title found');
+		is($book->{'author'},       'John Grisham'          ,'.. author found');
+		like($book->{'book_link'},  qr|http://www.thenile.com.au/books/John-Grisham/Ford-County/9780099547938/|);
+		is($book->{'image_link'},   'http://tncdn.net/1/978/009/9547938.jpg');
+		is($book->{'thumb_link'},   'http://tncdn.net/1/978/009/9547938.jpg');
+		like($book->{'description'},qr|John Grisham takes you into the heart of America's Deep South|);
+		is($book->{'publisher'},    'Cornerstone'           ,'.. publisher found');
+		is($book->{'pubdate'},      '2010'                  ,'.. pubdate found');
+		is($book->{'binding'},      'Paperback'             ,'.. binding found');
+		is($book->{'pages'},        '352'                   ,'.. pages found');
+		is($book->{'width'},        '111'                   ,'.. width found');
+		is($book->{'height'},       '178'                   ,'.. height found');
+		is($book->{'weight'},       undef                   ,'.. weight found');
+	}
+
+	$isbn = "9780007203055";
+	$record = $scraper->search($isbn);
+
+    unless($record->found) {
+		diag($record->error);
+    } else {
+		is($record->found,1);
+		is($record->found_in,'TheNile');
+
+		my $book = $record->book;
+		is($book->{'isbn'},         '9780007203055'         ,'.. isbn found');
+		is($book->{'isbn10'},       '0007203055'            ,'.. isbn10 found');
+		is($book->{'isbn13'},       '9780007203055'         ,'.. isbn13 found');
+		is($book->{'ean13'},        '9780007203055'         ,'.. ean13 found');
+		like($book->{'author'},     qr/Simon Ball/          ,'.. author found');
+		is($book->{'title'},        q|Bitter Sea|           ,'.. title found');
+		like($book->{'book_link'},  qr|http://www.thenile.com.au/books/Simon-Ball/Bitter-Sea/9780007203055/|);
+		is($book->{'image_link'},   'http://tncdn.net/1/978/000/7203055.jpg');
+		is($book->{'thumb_link'},   'http://tncdn.net/1/978/000/7203055.jpg');
+		like($book->{'description'},qr|The Mediterranean was indeed|);
+		is($book->{'pubdate'},      '2010'                  ,'.. pubdate found');
+		is($book->{'binding'},      'Paperback'             ,'.. binding found');
+		is($book->{'pages'},        416                     ,'.. pages found');
+		is($book->{'width'},        undef                   ,'.. width found');
+		is($book->{'height'},       undef                   ,'.. height found');
+		is($book->{'weight'},       321                     ,'.. weight found');
+
+        #use Data::Dumper;
+        #diag("book=[".Dumper($book)."]");
+	}
+
+    
+    $isbn = "9780718155896";
+	$record = $scraper->search($isbn);
+
+    unless($record->found) {
+		diag($record->error);
+    } else {
+		is($record->found,1);
+		is($record->found_in,'TheNile');
+
+		my $book = $record->book;
+		is($book->{'isbn'},         '9780718155896'         ,'.. isbn found');
+		is($book->{'isbn10'},       '0718155890'            ,'.. isbn10 found');
+		is($book->{'isbn13'},       '9780718155896'         ,'.. isbn13 found');
+		is($book->{'ean13'},        '9780718155896'         ,'.. ean13 found');
+		is($book->{'author'},       q|Clive Cussler, Justin Scott|  ,'.. author found');
+		is($book->{'title'},        q|The Spy|              ,'.. title found');
+		like($book->{'book_link'},  qr|http://www.thenile.com.au/books/Clive-Cussler-Justin-Scot/The-Spy/9780718155896/|);
+		is($book->{'image_link'},   'http://tncdn.net/1/978/071/8155896.jpg');
+		is($book->{'thumb_link'},   'http://tncdn.net/1/978/071/8155896.jpg');
+		like($book->{'description'},qr|international tensions are mounting as the world plunges towards war|);
+		is($book->{'pubdate'},      '2010'                  ,'.. pubdate found');
+		is($book->{'binding'},      'Paperback'             ,'.. binding found');
+		is($book->{'pages'},        436                     ,'.. pages found');
+		is($book->{'width'},        152                     ,'.. width found');
+		is($book->{'height'},       230                     ,'.. height found');
+		is($book->{'weight'},       undef                   ,'.. weight found');
+
+        #use Data::Dumper;
+        #diag("book=[".Dumper($book)."]");
+	}
+}
+
+###########################################################
+
+# crude, but it'll hopefully do ;)
+sub pingtest {
+  system("ping -q -c 1 www.google.com >/dev/null 2>&1");
+  my $retcode = $? >> 8;
+  # ping returns 1 if unable to connect
+  return $retcode;
+}
